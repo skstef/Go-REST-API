7e5b02ec6f3c909bbfc8b11b047499b6ec5d11f3
skstef <sstefstefv@gmail.com>
2024-10-25 23:59:05 +0300

    feature: event model setup

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/skstef/Go-REST-API/models"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the data"})
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
