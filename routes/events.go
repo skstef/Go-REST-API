1296ac664e654969887c2913f04ba9206239d487
skstef <sstefstefv@gmail.com>
2024-10-26 01:33:18 +0300

    feature: update event

--- routes/events.go
+++ routes/events.go
@@ -58,3 +58,38 @@ func CreateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
+
+func UpdateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the data"})
+		return
+	}
+
+	updateEvent.ID = eventId
+
+	err = updateEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully"})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", GetEvents)
 	server.GET("/events/:id", GetEvent)
 	server.POST("/events", CreateEvent)
+	server.PUT("/events/:id", UpdateEvent)
 }
