846feccbfcd165b076e4d33be365dd5bd92a2527
skstef <sstefstefv@gmail.com>
2024-10-28 23:30:48 +0200

    feature: auth middleware added

--- routes/events.go
+++ routes/events.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/skstef/Go-REST-API/models"
-	"github.com/skstef/Go-REST-API/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -39,22 +38,10 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
+	userId := context.GetInt64("userId")
 
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the data"})
